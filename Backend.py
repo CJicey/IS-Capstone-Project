7595192801b7c8f30122a8a82f57e86892a92107
Caleb Johnson <cjohn734@students.kennesaw.edu>
2025-02-23 14:32:30 -0500

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -3,6 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://myuser:mypassword@localhost/mydatabase'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')
