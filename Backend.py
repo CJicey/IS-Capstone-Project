3abb7711ef31e4050d449cb7f20f47cf4890ceab
Caleb Johnson <cjohn734@students.kennesaw.edu>
2025-03-03 17:47:37 -0500

    edited the code to go along with flask

--- Backend.py
+++ Backend.py
@@ -3,13 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://myuser:mypassword@localhost/mydatabase'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
     return render_template('index.html')
 
 @app.route('/checkout')
@@ -18,18 +17,26 @@ def checkout():
 
 @app.route('/prebrewed')
 def prebrewed():
-    return render_template('prebrewed.html')
+    return render_template("prebrewed")
 
 @app.route('/sale')
 def sale():
     return render_template('sale.html')
 
-@app.route('/Shop')
-def Shop():
-    return render_template('Shop.html')
+@app.route('/shop')
+def shop():
+    # Dummy product data for testing (Replace with database query)
+    products = [
+        {"name": "Coffee Beans", "image": "coffee.jpg", "price": 12.99},
+        {"name": "Espresso Machine", "image": "espresso.jpg", "price": 299.99}
+    ]
+    
+    categories = ["Coffee", "Machines", "Accessories"]  # Dummy category data
+    
+    return render_template('shop.html', products=products, categories=categories, cart_count=3)
 
-@app.route('/about')
-def about():
+@app.route('/about-us')
+def about_us():
     return render_template('about.html')
 
 if __name__ == '__main__':
