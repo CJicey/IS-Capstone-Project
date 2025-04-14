ea8b78aa7f91ef5343b6f26425d882a464bec967
Caleb Johnson <cjohn734@students.kennesaw.edu>
2025-04-13 23:13:38 -0400

    Update DB clean.py

--- Database/DB clean.py
+++ Database/DB clean.py
@@ -3,7 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Connect to MongoDB
 app.config["MONGO_URI"] = "mongodb+srv://CJicey:Baller10@cluster0.3dkrku9.mongodb.net/your_database_name"
 mongo = PyMongo(app)
 
